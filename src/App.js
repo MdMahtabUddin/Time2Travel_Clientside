094df5195bfc6d679bc6f31cb1d1fc8166f8ed5d
Md Mahtab Uddin <mahtab.sentu@outlook.com>
2021-10-28 23:50:50 +0600

    Added react router & bootstraop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter , Switch} from 'react-router-dom';
+import Navbar from './Pages/Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       
+
+
+       <BrowserRouter>
+<Navbar></Navbar>
+       <Switch>
+
+
+       </Switch>
+       </BrowserRouter>
     </div>
   );
 }
